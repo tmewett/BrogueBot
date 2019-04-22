37f4f0ed0d354bcec15c319613e839b4de7528ba
Tom M <tom@collider.in>
2019-04-22 10:42:04 +0100

    Test getworld with the bot - print where the player is before stepping

--- bin/brogue.lua
+++ bin/brogue.lua
@@ -1,3 +1,20 @@
+DCOLS = 79
+DROWS = 29
+
+function coords(cell)
+    cell = cell - 1
+    -- y is inner, x is outer
+    return cell // DROWS, cell % DROWS
+end
+
 function pushevents()
+    world = getworld()
+    for i=1, DCOLS*DROWS do
+        if world.flags[i] & 4 > 0 then
+            px, py = coords(i)
+            break
+        end
+    end
+    message(string.format("I am at (%d, %d)", px, py))
     stepto(math.random(0, 7))
 end
