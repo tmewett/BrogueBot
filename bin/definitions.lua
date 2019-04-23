1b706ce63aab6aa4be0be8b16f85dc94182cda12
Tom M <tom@collider.in>
2019-04-23 19:17:31 +0100

    Add terrain/tile flag enum definitions

--- bin/definitions.lua
+++ bin/definitions.lua
@@ -55,3 +55,38 @@ PERMANENT_TILE_FLAGS = (DISCOVERED | MAGIC_MAPPED | ITEM_DETECTED | HAS_ITEM | H
                         | IS_CHOKEPOINT | IS_GATE_SITE | IS_IN_MACHINE | IMPREGNABLE)
 
 ANY_KIND_OF_VISIBLE         = (VISIBLE | CLAIRVOYANT_VISIBLE | TELEPATHIC_VISIBLE)
+
+-- terrain/tile flags
+T_OBSTRUCTS_PASSABILITY         = Fl(0)        -- cannot be walked through
+T_OBSTRUCTS_VISION              = Fl(1)        -- blocks line of sight
+T_OBSTRUCTS_ITEMS               = Fl(2)        -- items can't be on this tile
+T_OBSTRUCTS_SURFACE_EFFECTS     = Fl(3)        -- grass, blood, etc. cannot exist on this tile
+T_OBSTRUCTS_GAS                 = Fl(4)        -- blocks the permeation of gas
+T_OBSTRUCTS_DIAGONAL_MOVEMENT   = Fl(5)        -- can't step diagonally around this tile
+T_SPONTANEOUSLY_IGNITES         = Fl(6)        -- monsters avoid unless chasing player or immune to fire
+T_AUTO_DESCENT                  = Fl(7)        -- automatically drops creatures down a depth level and does some damage (2d6)
+T_LAVA_INSTA_DEATH              = Fl(8)        -- kills any non-levitating non-fire-immune creature instantly
+T_CAUSES_POISON                 = Fl(9)        -- any non-levitating creature gets 10 poison
+T_IS_FLAMMABLE                  = Fl(10)       -- terrain can catch fire
+T_IS_FIRE                       = Fl(11)       -- terrain is a type of fire; ignites neighboring flammable cells
+T_ENTANGLES                     = Fl(12)       -- entangles players and monsters like a spiderweb
+T_IS_DEEP_WATER                 = Fl(13)       -- steals items 50% of the time and moves them around randomly
+T_CAUSES_DAMAGE                 = Fl(14)       -- anything on the tile takes max(1-2, 10%) damage per turn
+T_CAUSES_NAUSEA                 = Fl(15)       -- any creature on the tile becomes nauseous
+T_CAUSES_PARALYSIS              = Fl(16)       -- anything caught on this tile is paralyzed
+T_CAUSES_CONFUSION              = Fl(17)       -- causes creatures on this tile to become confused
+T_CAUSES_HEALING                = Fl(18)       -- heals 20% max HP per turn for any player or non-inanimate monsters
+T_IS_DF_TRAP                    = Fl(19)       -- spews gas of type specified in fireType when stepped on
+T_CAUSES_EXPLOSIVE_DAMAGE       = Fl(20)       -- is an explosion; deals higher of 15-20 or 50% damage instantly, but not again for five turns
+T_SACRED                        = Fl(21)       -- monsters that aren't allies of the player will avoid stepping here
+
+T_OBSTRUCTS_SCENT               = (T_OBSTRUCTS_PASSABILITY | T_OBSTRUCTS_VISION | T_AUTO_DESCENT | T_LAVA_INSTA_DEATH | T_IS_DEEP_WATER | T_SPONTANEOUSLY_IGNITES)
+T_PATHING_BLOCKER               = (T_OBSTRUCTS_PASSABILITY | T_AUTO_DESCENT | T_IS_DF_TRAP | T_LAVA_INSTA_DEATH | T_IS_DEEP_WATER | T_IS_FIRE | T_SPONTANEOUSLY_IGNITES)
+T_DIVIDES_LEVEL                 = (T_OBSTRUCTS_PASSABILITY | T_AUTO_DESCENT | T_IS_DF_TRAP | T_LAVA_INSTA_DEATH | T_IS_DEEP_WATER)
+T_LAKE_PATHING_BLOCKER          = (T_AUTO_DESCENT | T_LAVA_INSTA_DEATH | T_IS_DEEP_WATER | T_SPONTANEOUSLY_IGNITES)
+T_WAYPOINT_BLOCKER              = (T_OBSTRUCTS_PASSABILITY | T_AUTO_DESCENT | T_IS_DF_TRAP | T_LAVA_INSTA_DEATH | T_IS_DEEP_WATER | T_SPONTANEOUSLY_IGNITES)
+T_MOVES_ITEMS                   = (T_IS_DEEP_WATER | T_LAVA_INSTA_DEATH)
+T_CAN_BE_BRIDGED                = (T_AUTO_DESCENT)
+T_OBSTRUCTS_EVERYTHING          = (T_OBSTRUCTS_PASSABILITY | T_OBSTRUCTS_VISION | T_OBSTRUCTS_ITEMS | T_OBSTRUCTS_GAS | T_OBSTRUCTS_SURFACE_EFFECTS | T_OBSTRUCTS_DIAGONAL_MOVEMENT)
+T_HARMFUL_TERRAIN               = (T_CAUSES_POISON | T_IS_FIRE | T_CAUSES_DAMAGE | T_CAUSES_PARALYSIS | T_CAUSES_CONFUSION | T_CAUSES_EXPLOSIVE_DAMAGE)
+T_RESPIRATION_IMMUNITIES        = (T_CAUSES_DAMAGE | T_CAUSES_CONFUSION | T_CAUSES_PARALYSIS | T_CAUSES_NAUSEA)
